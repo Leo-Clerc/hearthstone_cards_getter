1c37d2d8ca20e43f77af1fda014f31772712e5cb
Leo-Clerc <leocoursadistance@gmail.com>
2026-02-23 00:39:34 +0100

    Add script

---
+++ getCards.py
@@ -0,0 +1,11 @@
+import requests 
+import pandas as pd
+
+url = "https://api.hearthstonejson.com/v1/190920/enUS/cards.collectible.json"
+
+response = requests.get(url)
+response.encoding="utf-8"
+data = response.json()
+
+df = pd.DataFrame.from_dict(data)
+csv = df.to_csv("hearthstone_card.csv", encoding="utf-8", index=False)
